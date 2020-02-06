45ad5ff91a023eae3ed68adf2c3dcceda7ed0a75
xors <obivan05@gmail.com>
2020-02-06 12:22:30 +0100

    working model

--- catalog/models.py
+++ catalog/models.py
@@ -12,7 +12,7 @@ class Genre(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey('Author', on_delete='models.SET_NULL', null=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000, help_text='Brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, help_text="13 character")
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
@@ -25,7 +25,7 @@ class Book(models.Model):
 
 class BookInstance(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for the particular book')
-    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
 
@@ -36,10 +36,33 @@ class BookInstance(models.Model):
         ('r', 'Reserved'),
     )
 
+    LANGUAGE = (
+        ('en', 'English'),
+        ('hu', 'Hungarian'),
+        ('de', 'German'),
+    )
+
     status = models.CharField(max_length=1,choices=LOAN_STATUS,blank=True,default='m',help_text="Book availability")
+
+    language = models.CharField(max_length=2, choices=LANGUAGE,blank=True, default='en', help_text="Book language")
 
     class Meta:
         ordering = ['due_back']
 
     def __str__(self):
         return f'{self.id} ({self.book.title})'
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
