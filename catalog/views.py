90d2a18dfeaff49d9eeb540d862ca76deed50473
xors <obivan05@gmail.com>
2020-02-10 09:52:16 +0100

    creating views

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from catalog.models import Book, BookInstance, Author
 
 # Create your views here.
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact = 'a').count()
+    num_authors = Author.objects.count() # all() is implied by default
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances, 
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
