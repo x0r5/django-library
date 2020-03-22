a4706dc88c2405b61315c6b8d3becd6b32611b2d
x0r5 <obivan05@gmail.com>
2020-03-22 22:13:30 +0100

    debug

--- catalog/forms.py
+++ catalog/forms.py
@@ -6,12 +6,12 @@ import datetime
 class RenewBookForm(forms.Form):
     renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (defalut 3)")
 
-    def clear_renewal_date(self):
-        data = self.cleaned_data('renewal_date')
+    def clean_renewal_date(self):
+        data = self.cleaned_data['renewal_date']
 
         if data < datetime.date.today():
             raise ValidationError(_('Invalid date - renewal in past'))
         if data > datetime.date.today() + datetime.timedelta(weeks=4):
-            raise ValidateionError(_('Invalid date - renewal more than 4 weeks ahead'))
+            raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
 
         return data
